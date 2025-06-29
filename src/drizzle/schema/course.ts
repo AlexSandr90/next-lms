eed723d65977aaa5ada44f8e0c14f700d192a99e
Oleksandr <gayrsuk@gmail.com>
2025-06-30 01:37:42 +0300

    Added Drizzle

---
+++ src/drizzle/schema/course.ts
@@ -0,0 +1,16 @@
+import { relations } from "drizzle-orm";
+import { pgTable, text } from "drizzle-orm/pg-core";
+import { id, createdAt, updatedAt } from "../schemaHelpers";
+
+
+export const CourseTable = pgTable('courses', {
+    id,
+    name: text().notNull(),
+    description: text().notNull(),
+    createdAt,
+    updatedAt,
+})
+
+export const CourseRelationships = relations(CourseTable, ({ one, many }) => {
+    test: one()
+})
