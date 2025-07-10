4016cf24869ddccee1cc072f63fb0dcb73b2b51c
Oleksandr <gayrsuk@gmail.com>
2025-07-10 23:38:16 +0300

    Update courseSection.ts

--- src/drizzle/schema/courseSection.ts
+++ src/drizzle/schema/courseSection.ts
@@ -2,6 +2,7 @@ import { integer, pgEnum, pgTable, text, uuid } from "drizzle-orm/pg-core";
 import { id, createdAt, updatedAt } from "../schemaHelpers";
 import { CourseTable } from "./course";
 import { relations } from "drizzle-orm";
+import { LessonTable } from "./lesson";
 
 export const courseSectionStatuses = ['public', 'private'] as const;
 export type CourseSectionStatus = (typeof courseSectionStatuses)[number];
@@ -28,6 +29,7 @@ export const CourseSectionsRelationShips = relations(
         course: one(CourseTable, {
             fields: [CourseSectionTable.courseId],
             references: [CourseTable.id]
-        })
+        }),
+        lessons: many(LessonTable)
     })
 )
