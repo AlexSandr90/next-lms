352b1795141c545f95429ac5941ae1e1ae13e6bc
Oleksandr <gayrsuk@gmail.com>
2025-07-28 23:31:16 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import {db} from "@/drizzle/db";
 import {cacheTag} from "next/dist/server/use-cache/cache-tag";
+import ConsumerLayout from "@/app/(consumer)/layout";
 
 export default async function HomePage() {
 
     return (
         <div className="container my-6">
-            <div className='grid grid-cols-[repeat(auto-fill, minmax(300px, 1fr))] gap-4'>
-                <h1>HI</h1>
-            </div>
+            <ConsumerLayout>
+                <div className='grid grid-cols-[repeat(auto-fill, minmax(300px, 1fr))] gap-4'>
+                    <h1>HI</h1>
+                </div>
+            </ConsumerLayout>
         </div>
     )
 }
 
 
 async function getPublicProducts() {
-    'use cache'
+    // 'use cache'
     // cacheTag(getProductGlobalTag())
 }
