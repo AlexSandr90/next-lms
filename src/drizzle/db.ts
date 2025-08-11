ae10a50a653e35adcfa16ab6b79b21d3bd696c61
Oleksandr <gayrsuk@gmail.com>
2025-08-12 00:25:30 +0300

    Update db.ts

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,11 +3,11 @@ import * as schema from './schema'
 import {env} from "@/data/env/server";
 
 export const db = drizzle({
-    schema,
-    connection: {
-        password: env.DB_PASSWORD,
-        user: env.DB_USER,
-        database: env.DB_NAME,
-        host: env.DB_HOST,
-    }
+  schema,
+  connection: {
+    password: env.DB_PASSWORD,
+    user: env.DB_USER,
+    database: env.DB_NAME,
+    host: env.DB_HOST,
+  }
 })
